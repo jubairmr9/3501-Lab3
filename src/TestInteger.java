ae2ab3a8bc2d8ff194dd25a1b5e12eede061174e
Jubair <hassa357@morris.umn.edu>
2018-12-20 23:54:46 -0600

    Add TestInteger class.

--- src/TestInteger.java
+++ src/TestInteger.java
@@ -1,2 +1,44 @@
-public class TestInteger {
+import java.util.Arrays;
+
+public class TestInteger implements Comparable<TestInteger> {
+
+    public int value;
+
+    static long counter;
+
+    // Takes in an integer indexSize and sets it to value.
+
+    TestInteger(int indexSize){
+
+        value = indexSize;
+
+    }
+
+    // Compares the numbers to check if they are greater than, equal to or less than
+    // each other and returns a value. Also it adds to the counter every time it loops
+    // through and compares another number.
+
+    public int compareTo(TestInteger valueToCompare) {
+
+        int less = -1;
+        int eq = 0;
+        int greater = 1;
+        counter++;
+
+        if (this.value < valueToCompare.value){
+
+            return less;
+
+        }
+
+        if (this.value == valueToCompare.value){
+
+            return eq;
+
+        }
+
+        else return greater;
+
+    }
+
 }
